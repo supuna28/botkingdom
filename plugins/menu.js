64da7be66114b12a43b671b79db3bea2612454c6
supuna <92195424+supuna28@users.noreply.github.com>
2021-11-11 13:38:25 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -720,7 +720,7 @@ let img = fs.readFileSync('src/donasi.jpg')
 
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'MADE WITH ❤️ BOT KINGDOM  UPTime: *%time*', 'clone', '.jadibot', 'BOTKINGDOM', '.donasi', m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'MADE WITH ❤️ BOT KINGDOM  UPTime: *%time', 'clone', '.jadibot', 'BOTKINGDOM', '.donasi', m)
 
   } catch (e) {
 

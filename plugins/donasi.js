1882ba88cf13819fbf1e0c7a564f09020cc1202a
supuna <92195424+supuna28@users.noreply.github.com>
2021-11-13 08:01:12 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -5,9 +5,7 @@ let fs = require('fs')
 let handler = async (m, { conn }) => {
 
 let img = fs.readFileSync('src/donasi.jpg')
-
-conn.sendLoc(m.chat, img, `*[+] BOT KINGDOM [+]*
-
+conn.sendLoc ( m . chat ,  img ,  `*[+] SOURCE CODE [+]*
 Link GROUP:
 
 https://chat.whatsapp.com/CIDWlH7yzEsKombRDcTKcV`, global.footer, 'BACK MENU', '#menu', m)
